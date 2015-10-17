485aef18ffe7ff4b1d0bc6fabb0488ea753c7981
Alex Vermeulen <alexvermeulen@me.com>
2015-10-17 13:23:11 -0400

    Save exit status before conditionals
    The if statments change the exit status, affecting the prompt output.

--- include/appearance.sh
+++ include/appearance.sh
@@ -12,7 +12,7 @@ CYAN="\[\033[0;36m\]"
 WHITE="\[\033[0;37m\]"
 BR_RED="\[\033[1;31m\]"
 BR_MAGENTA="\[\033[1;35m\]"
-RESET="\[\033[0m\]"
+RESET="\[\e[0m\]"
 
 # Grep
 export GREP_OPTIONS='--color=auto'
@@ -23,41 +23,43 @@ if [[ $PLATFORM == 'mac' ]]; then
     export LSCOLORS='Gxfxcxdxdxegedabagacad'
 
     # Load bash_completion for __git_ps1
-    if [ ! -f $(brew --prefix)/etc/bash_completion ]; then
+    if [ ! -f "$(brew --prefix)/etc/bash_completion" ]; then
         brew install bash-completion
     fi
-    source $(brew --prefix)/etc/bash_completion
+    source "$(brew --prefix)/etc/bash_completion"
 
 elif [[ $PLATFORM == 'linux' ]]; then
     # Load bash_git for __git_ps1
-    if [ ! -f $HOME/.bash_git ]; then
-        pushd $HOME
+    if [ ! -f "$HOME/.bash_git" ]; then
+        pushd "$HOME"
         wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
         mv git-prompt.sh .bash_git
         popd
     fi
-    source $HOME/.bash_git
+    source "$HOME/.bash_git"
 fi
 
 # Customize the prompt
 function exitstatus {
+    local exit="$?" # Save exit status since the if statments below will change it.
+    local host
+    local venv
+    local prompt
+
     if [[ -n $SSH_CLIENT ]]; then
-        host=@$WHITE`echo $HOSTNAME | cut -d '.' -f 1`$RESET
-    else
-        host=''
+        host=@$WHITE$(echo "$HOSTNAME" | cut -d '.' -f 1)$RESET
     fi
 
     if [[ $VIRTUAL_ENV != '' ]]; then
         venv="$WHITE(${VIRTUAL_ENV##*/}) $RESET"
-    else
-        venv=''
     fi
 
-    if [ $? -eq 0 ]
+    prompt=" \u${host}:\W$GREEN$(__git_ps1)$RESET\n$ "
+    if [ $exit -eq 0 ]
     then
-        PS1="${venv}$GREEN:)$RESET $CYAN\D{%H:%M:%S}$RESET \u${host}:\W$GREEN$(__git_ps1)$RESET\n\[\e[0m\]\$ "
+        PS1="${venv}$GREEN:) $CYAN\D{%H:%M:%S}$RESET${prompt}"
     else
-        PS1="${venv}$RED:(${venv}$RESET $YELLOW\D{%H:%M:%S}$RESET \u${host}:\W$GREEN$(__git_ps1)$RESET\n\[\e[0m\]\$ "
+        PS1="${venv}$RED:( $YELLOW\D{%H:%M:%S}$RESET${prompt}"
     fi
 }
 PROMPT_COMMAND=exitstatus
